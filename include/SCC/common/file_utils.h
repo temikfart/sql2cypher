7ec5744de0cbf9875ca90e774d0f650703cd2355
Artyom Fartygin <fartygin.a@phystech.edu>
2023-10-03 23:45:33 +0300

    [#23] Fix: move constexpr function to the header

--- include/SCC/common/file_utils.h
+++ include/SCC/common/file_utils.h
@@ -4,6 +4,12 @@
 
 namespace scc::common {
 
-constexpr std::string ResourcesPath();
+constexpr std::string ResourcesPath() {
+  std::string current_file = __FILE__;
+  std::string filename = current_file.substr(current_file.find_last_of('/') + 1);
+  std::string current_dir = current_file.substr(0, current_file.find(filename));
+
+  return current_dir + "../../resources";
+}
 
 } // scc::common
