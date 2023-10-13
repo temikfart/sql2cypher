59ebcabf3da381c7d4811458a07341505a37b8cb
Artyom Fartygin <fartygin.a@phystech.edu>
2023-10-13 13:25:30 +0300

    [#23] Fix resources path method

--- src/common/file_utils.cpp
+++ src/common/file_utils.cpp
@@ -1,4 +1,13 @@
 #include "SCC/common/file_utils.h"
 
 namespace scc::common {
+
+std::string ResourcesPath() {
+  std::string current_file = __FILE__;
+  std::string filename = current_file.substr(current_file.find_last_of('/') + 1);
+  std::string current_dir = current_file.substr(0, current_file.find(filename));
+
+  return current_dir + "../../resources";
+}
+
 } // scc::common
