38d76cd53f85884a59feccb20d537996937d3bf6
Artyom Fartygin <fartygin.a@phystech.edu>
2023-10-21 16:28:13 +0300

    [#23] Add namespace to logger wrapper

--- include/SCC/log.h
+++ include/SCC/log.h
@@ -5,14 +5,11 @@
 
 #include "logger/log.hpp"
 
-// TODO: change to namespace without class.
-class SCCLogger {
-public:
-  static void set_severity(logger::Severity severity);
-  static void set_severity(const std::string& str_severity);
-  static logger::Severity to_severity(const std::string& str_severity);
-  static std::optional<logger::Severity> get_severity();
+namespace scc::log {
 
-private:
-  static void ValidateSeverity(logger::Severity severity);
-};
+void set_severity(logger::Severity severity);
+void set_severity(const std::string& str_severity);
+logger::Severity to_severity(const std::string& str_severity);
+std::optional<logger::Severity> get_severity();
+
+} // scc::log
--- src/log.cpp
+++ src/log.cpp
@@ -1,25 +1,35 @@
 #include "SCC/log.h"
 
-void SCCLogger::set_severity(logger::Severity severity) {
-  SCCLogger::ValidateSeverity(severity);
-  logger::Logger::get()->set_severity(severity);
-}
-void SCCLogger::set_severity(const std::string& str_severity) {
-  logger::Severity new_severity = to_severity(str_severity);
-  set_severity(new_severity);
-}
-logger::Severity SCCLogger::to_severity(const std::string& str_severity) {
-  return logger::to_severity(str_severity);
-}
-std::optional<logger::Severity> SCCLogger::get_severity() {
-  logger::AppenderType appender_type = logger::AppenderType::console;
-  return logger::Logger::get()->severity(appender_type);
-}
+using namespace logger;
 
-void SCCLogger::ValidateSeverity(logger::Severity severity) {
-  if (logger::Severity::trace < severity) {
+namespace {
+
+void ValidateSeverity(Severity severity) {
+  if (Severity::trace < severity) {
     std::cerr << "incorrect SCC log level: "
               << std::to_string(severity) << std::endl;
     exit(EXIT_FAILURE);
   }
 }
+
+}
+
+namespace scc::log {
+
+void set_severity(Severity severity) {
+  ValidateSeverity(severity);
+  Logger::get()->set_severity(severity);
+}
+void set_severity(const std::string& str_severity) {
+  Severity new_severity = to_severity(str_severity);
+  set_severity(new_severity);
+}
+Severity to_severity(const std::string& str_severity) {
+  return logger::to_severity(str_severity);
+}
+std::optional<Severity> get_severity() {
+  AppenderType appender_type = AppenderType::console;
+  return Logger::get()->severity(appender_type);
+}
+
+} // scc::log
