5e1d6db8e911776355f1ff84ebddfab42cc85311
Artyom Fartygin <fartygin.a@phystech.edu>
2023-10-22 23:46:35 +0300

    [#23] Add error handling in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,35 @@
 #include "SCC/main.h"
 
 int main(int argc, char* argv[]) {
-  scc::SCCArgs scc_args;
-  scc_args.ParseArgs(argc, argv);
+  try {
+    scc::SCCArgs scc_args;
+    scc_args.ParseArgs(argc, argv);
 
-  scc::InitConfig(scc_args);
-  scc::SCCConfig* config = scc::SCCConfig::Get();
+    scc::InitConfig(scc_args);
+    scc::SCCConfig* config = scc::SCCConfig::Get();
 
-  logger::init(config->log_severity, config->log_directory);
+    logger::init(config->log_severity, config->log_directory);
 
-  Tokenizer tokenizer(config->get_sql_file());
-  tokenizer.Tokenize();
+    Tokenizer tokenizer(config->get_sql_file());
+    tokenizer.Tokenize();
 
-  SyntaxAnalyzer syntax_analyzer;
-  std::shared_ptr<Node> AST =
-      syntax_analyzer.Analyze(tokenizer.get_tokens_array());
+    SyntaxAnalyzer syntax_analyzer;
+    std::shared_ptr<Node> AST =
+        syntax_analyzer.Analyze(tokenizer.get_tokens_array());
 
-  if (scc_args.IsUsed("--dump")) {
-    TreeDump dump(config->get_ast_dump_file());
-    dump.DumpTree(AST);
+    if (scc_args.IsUsed("--dump")) {
+      TreeDump dump(config->get_ast_dump_file());
+      dump.DumpTree(AST);
+    }
+
+    QueryAssembler query_assembler(config->get_cypher_file());
+    query_assembler.Translate(AST);
+
+    end(EXIT_SUCCESS);
+  } catch (const std::exception& e) {
+    std::cerr << "Translation failed with error." << std::endl;
+    std::cerr << e.what() << std::endl;
   }
-
-  QueryAssembler query_assembler(config->get_cypher_file());
-  query_assembler.Translate(AST);
-
-  end(EXIT_SUCCESS);
 
   return 0;
 }
