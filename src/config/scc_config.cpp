36776a113473fbf483cd088e984c535f1d05689f
Artyom Fartygin <fartygin.a@phystech.edu>
2023-10-22 23:46:24 +0300

    [#23] Refactor Config ctor

--- src/config/scc_config.cpp
+++ src/config/scc_config.cpp
@@ -4,26 +4,42 @@ void end(int exit_code) {
   exit(exit_code);
 }
 
+namespace {
+
+inline void ThrowFailedConfigCreation(const std::exception& e) {
+  throw std::runtime_error("Failed to create configuration: " + std::string(e.what()));
+}
+
+}
+
 namespace scc {
 
 namespace fs = std::filesystem;
 
+SCCConfig::SCCConfig(const SCCArgs& args) {
+  try {
+    log_severity = args.Get<logger::Severity>("--log-severity");
+    log_directory = fs::weakly_canonical(args.Get("--log-directory"));
+    mode = args.Get<SCCMode>("--mode");
+
+    std::string sql_file_path = args.Get("--sql");
+    common::ValidateFileExists(sql_file_path);
+    sql_file_ = fs::canonical(sql_file_path);
+    cypher_file_ = fs::weakly_canonical(args.Get("--cypher"));
+    if (args.IsUsed("--dump"))
+      ast_dump_file_ = fs::weakly_canonical(args.Get("--dump"));
+  } catch (const std::logic_error& e) {
+    ThrowFailedConfigCreation(e);
+  } catch (const std::runtime_error& e) {
+    ThrowFailedConfigCreation(e);
+  }
+}
+
 SCCConfig* SCCConfig::Get() {
   auto* instance = get_instance();
   if (instance == nullptr)
-    std::cerr << "SCC: ERROR! Could not start: the configuration is not initialized." << std::endl;
+    throw std::runtime_error("The configuration has not been initialized yet");
   return instance;
-}
-
-void SCCConfig::SetParamsFrom(const SCCArgs& args) {
-  log_severity = args.Get<logger::Severity>("--log-severity");
-  log_directory = fs::weakly_canonical(args.Get("--log-directory"));
-  mode = SCCMode(args.Get<SCCMode>("--mode"));
-
-  sql_file_ = fs::canonical(args.Get("--sql"));
-  cypher_file_ = fs::weakly_canonical(args.Get("--cypher"));
-  if (args.IsUsed("--dump"))
-    ast_dump_file_ = fs::weakly_canonical(args.Get("--dump"));
 }
 
 const fs::path& SCCConfig::get_sql_file() const {
@@ -36,10 +52,8 @@ const fs::path& SCCConfig::get_ast_dump_file() const {
   return ast_dump_file_;
 }
 
-
 SCCConfig& InitConfig(const SCCArgs& args) {
-  static SCCConfig scc_config;
-  scc_config.SetParamsFrom(args);
+  static SCCConfig scc_config(args);
   return scc_config;
 }
 
