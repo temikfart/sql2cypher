07ae0461d545cd842f3d3d61e2dbe1773724d048
Artyom Fartygin <fartygin.a@phystech.edu>
2023-10-22 23:15:56 +0300

    [#23] Simplify LowerCase function

--- src/common/string_utils.cpp
+++ src/common/string_utils.cpp
@@ -3,12 +3,9 @@
 namespace scc::common {
 
 std::string LowerCase(const std::string& str) {
-  std::string result = str;
-  std::transform(result.begin(), result.end(), result.begin(),
-                 [](unsigned char c) {
-                   return std::tolower(c);
-                 });
-  return result;
+  std::string res = str;
+  std::transform(res.begin(), res.end(), res.begin(), tolower);
+  return res;
 }
 
 } // scc::common
