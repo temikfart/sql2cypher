ba1218f25dc8d90ab32789c68be4ff817ac5b8c9
Artyom Fartygin <fartygin.a@phystech.edu>
2023-10-27 19:22:42 +0300

    [#23] Add custom config tests

--- test/fixtures/src/scc_config_fixtures.cpp
+++ test/fixtures/src/scc_config_fixtures.cpp
@@ -10,3 +10,27 @@ void SCCConfigBaseTests::TearDown() {
   SCCArgsTests::TearDown();
 }
 
+void SCCConfigTests::InitializeConfig() {
+  InitConfig(parser);
+  config = SCCConfig::Get();
+}
+
+void SCCConfigTests::SetUp() {
+  SCCConfigBaseTests::SetUp();
+  InitializeConfig();
+}
+void SCCConfigTests::TearDown() {
+  SCCConfigBaseTests::TearDown();
+}
+
+void CustomSCCConfigTests::InitializeConfig() {
+  InitConfig(parser);
+  config = SCCConfig::Get();
+}
+
+void CustomSCCConfigTests::SetUp() {
+  SCCArgsBaseTests::SetUp();
+}
+void CustomSCCConfigTests::TearDown() {
+  SCCArgsBaseTests::TearDown();
+}
