f1e61b787fb1e89f9973823b504b022cda215fb7
Artyom Fartygin <fartygin.a@phystech.edu>
2023-10-28 17:54:08 +0300

    [#23] Remove std::format from tests
    g++ in GitHub Actions does not support this feature.

--- test/fixtures/src/scc_args_fixtures.cpp
+++ test/fixtures/src/scc_args_fixtures.cpp
@@ -5,9 +5,25 @@ using namespace scc;
 void SCCArgsBaseTests::ParseArgsWrapper() {
   parser.ParseArgs(args.argc, args.argv.data());
 }
+char* SCCArgsBaseTests::CreateArg(const std::string& option, const std::string& value) {
+  char delimiter[] = "=";
+  char* arg = new char[option.length() + value.length() + sizeof(delimiter) + 1];
+
+  strcpy(arg, option.c_str());
+  strcat(arg, delimiter);
+  strcat(arg, value.c_str());
+
+  return arg;
+}
 void SCCArgsBaseTests::AddArg(const std::string& option) {
   char* arg = new char[option.length() + 1];
   strcpy(arg, option.c_str());
+
+  args.argv.push_back(arg);
+  args.argc++;
+}
+void SCCArgsBaseTests::AddArg(const std::string& option, const std::string& value) {
+  char* arg = CreateArg(option, value);
 
   args.argv.push_back(arg);
   args.argc++;
@@ -29,7 +45,7 @@ void SCCArgsBaseTests::TearDown() {
 }
 
 void SCCArgsTests::AddSqlArgWithDefaultValue() {
-  AddArg(std::format("--sql={}", sql_path));
+  AddArg("--sql", sql_path);
 }
 
 void SCCArgsTests::SetUp() {
