4152dd55c5ddc063f80bc5f9cb59beb9f302b6f6
Artyom Fartygin <fartygin.a@phystech.edu>
2024-03-23 20:23:23 +0300

    [#31] Add DataType unit-tests for strings

--- test/ast/data_types_test.cpp
+++ test/ast/data_types_test.cpp
@@ -1,3 +1,5 @@
+#include <sstream>
+
 #include "gtest/gtest.h"
 
 #include "SCC/ast/data_types.h"
@@ -40,6 +42,33 @@ TEST(DataTypeCtorTests, InvalidValueTest) {
   EXPECT_THROW(DataType(DataType::Value(100)), std::invalid_argument);
 }
 
+TEST(DataTypeCtorTests, StringValueTest) {
+  DataType root(DT_ROOT);
+  EXPECT_EQ(DataType::kRoot, (DataType::Value) root);
+
+  DataType punctuation(DT_PUNCTUATION);
+  EXPECT_EQ(DataType::kPunctuation, (DataType::Value) punctuation);
+
+  DataType mixed_case_service("serViCE");
+  EXPECT_EQ(DataType::kService, (DataType::Value) mixed_case_service);
+}
+
+TEST(DataTypeCtorTests, InvalidStringValueTest) {
+  EXPECT_THROW(DataType("something wrong"), std::invalid_argument);
+  EXPECT_THROW(DataType("oa8sdg"), std::invalid_argument);
+  EXPECT_THROW(DataType("ro0t"), std::invalid_argument);
+  EXPECT_THROW(DataType("non"), std::invalid_argument);
+  EXPECT_THROW(DataType("braket"), std::invalid_argument);
+}
+
+TEST(DataTypeoStringTests, ToStringTest) {
+  EXPECT_EQ(DT_NONE, DataType(DataType::kNone).ToString());
+  EXPECT_EQ(DT_ROOT, DataType(DataType::kRoot).ToString());
+  EXPECT_EQ(DT_INT, DataType(DataType::kInt).ToString());
+  EXPECT_EQ(DT_SERVICE, DataType(DataType::kService).ToString());
+  EXPECT_EQ(DT_WORD, DataType(DataType::kWord).ToString());
+}
+
 TEST(DataTypeCastTests, CastToValueTest) {
   DataType bracket(DataType::kBracket);
   auto bracket_val = (DataType::Value) bracket;
@@ -48,6 +77,24 @@ TEST(DataTypeCastTests, CastToValueTest) {
   DataType punctuation(DataType::kPunctuation);
   auto punctuation_val = (DataType::Value) punctuation;
   EXPECT_EQ(DataType::kPunctuation, punctuation_val);
+}
+
+TEST(DataTypeOperatorsTests, OutputTest) {
+  std::ostringstream oss_none;
+  oss_none << DataType(DataType::kNone);
+  EXPECT_EQ(DT_NONE, oss_none.str());
+
+  std::ostringstream oss_punctuation;
+  oss_punctuation << DataType(DataType::kPunctuation);
+  EXPECT_EQ(DT_PUNCTUATION, oss_punctuation.str());
+
+  std::ostringstream oss_operator;
+  oss_operator << DataType(DataType::kOperator);
+  EXPECT_EQ(DT_OPERATOR, oss_operator.str());
+
+  std::ostringstream oss_string;
+  oss_string << DataType(DataType::kString);
+  EXPECT_EQ(DT_STRING, oss_string.str());
 }
 
 TEST(DataTypeOperatorsTests, CompareTwoEqualTypesTest) {
