36f86b80a2a0445be9d6d19e8e83aa14e9f25031
Artyom Fartygin <fartygin.a@phystech.edu>
2024-03-26 16:39:17 +0300

    [#31] Add SymbolTypes enum

---
+++ src/lexer/symbol_classifier.cpp
@@ -0,0 +1,52 @@
+#include "SCC/lexer/symbol_classifier.h"
+
+SymbolType SymbolClassifier::DefineSymbolType(char c) {
+  if (IsSpace(c))
+    return SymbolType::kSpace;
+  if (IsDigit(c))
+    return SymbolType::kDigit;
+  if (IsAlpha(c))
+    return SymbolType::kAlpha;
+  if (IsOperator(c))
+    return SymbolType::kOperator;
+  if (IsBracket(c))
+    return SymbolType::kBracket;
+  if (IsPunctuation(c))
+    return SymbolType::kOperator;
+  if (IsEOF(c))
+    return SymbolType::kEOF;
+  if (IsNullTerminator(c))
+    return SymbolType::kNullTerminator;
+
+  return SymbolType::kUnknown;
+}
+bool SymbolClassifier::IsSpace(char c) {
+  return std::isspace(c) != 0;
+}
+bool SymbolClassifier::IsDigit(char c) {
+  return std::isdigit(c) != 0;
+}
+bool SymbolClassifier::IsAlpha(char c) {
+  return std::isalpha(c) != 0;
+}
+bool SymbolClassifier::IsOperator(char c) {
+  return IsCharFromString(c, "+-*/^!=<>");
+}
+bool SymbolClassifier::IsBracket(char c) {
+  return IsCharFromString(c, "(){}[]");
+}
+bool SymbolClassifier::IsPunctuation(char c) {
+  return IsCharFromString(c, ":;.,\'\"");
+}
+bool SymbolClassifier::IsNullTerminator(char c) {
+  return c == '\0';
+}
+bool SymbolClassifier::IsEOF(char c) {
+  return c == EOF;
+}
+
+bool SymbolClassifier::IsCharFromString(char c, const std::string& str) {
+  return std::any_of(str.begin(), str.end(), [c](char op) {
+    return (c == op);
+  });
+}
