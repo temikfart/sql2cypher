65eb5a2248560671e4f69dd46d8128ba9c5cb9f5
Artyom Fartygin <fartygin.a@phystech.edu>
2024-03-26 18:00:42 +0300

    [#31] Add tests templates

---
+++ test/lexer/symbol_classifier_test.cpp
@@ -0,0 +1,8 @@
+#include "gtest/gtest.h"
+
+#include "SCC/lexer/symbol_classifier.h"
+
+//using namespace scc;
+using namespace testing;
+
+// TODO
---
+++ test/lexer/symbol_types_test.cpp
@@ -0,0 +1,8 @@
+#include "gtest/gtest.h"
+
+#include "SCC/lexer/symbol_types.h"
+
+//using namespace scc;
+using namespace testing;
+
+// TODO
