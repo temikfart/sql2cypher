11d37048e47346797f65b0b44d4b4ab128212d92
Artyom Fartygin <fartygin.a@phystech.edu>
2024-04-20 13:30:36 +0300

    [#36] Refactor NodeDataClassifier class

--- include/SCC/parser/common/node_data_classifier.h
+++ include/SCC/parser/common/node_data_classifier.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "SCC/ast/data_types.h"
 #include "SCC/ast/nodes/inode.h"
 #include "SCC/parser/common/utils.h"
 
@@ -7,16 +8,19 @@ namespace scc::parser::common {
 
 class NodeDataClassifier {
 public:
-  static bool IsDot(std::shared_ptr<ast::INode>& node);
-  static bool IsComma(std::shared_ptr<ast::INode>& node);
-  static bool IsOpeningRoundBracket(std::shared_ptr<ast::INode>& node);
-  static bool IsClosingRoundBracket(std::shared_ptr<ast::INode>& node);
-  static bool IsSingleQuote(std::shared_ptr<ast::INode>& node);
-  static bool IsDoubleQuote(std::shared_ptr<ast::INode>& node);
-  static bool IsQuote(std::shared_ptr<ast::INode>& node);
-  static bool IsUnaryOperator(std::shared_ptr<ast::INode>& node);
-  static bool IsBinaryOperator(std::shared_ptr<ast::INode>& node);
-  static bool IsSemicolon(std::shared_ptr<ast::INode>& node);
+  static bool IsDot(const std::shared_ptr<ast::INode>& node);
+  static bool IsComma(const std::shared_ptr<ast::INode>& node);
+  static bool IsOpeningRoundBracket(const std::shared_ptr<ast::INode>& node);
+  static bool IsClosingRoundBracket(const std::shared_ptr<ast::INode>& node);
+  static bool IsSingleQuote(const std::shared_ptr<ast::INode>& node);
+  static bool IsDoubleQuote(const std::shared_ptr<ast::INode>& node);
+  static bool IsQuote(const std::shared_ptr<ast::INode>& node);
+  static bool IsUnaryOperator(const std::shared_ptr<ast::INode>& node);
+  static bool IsBinaryOperator(const std::shared_ptr<ast::INode>& node);
+  static bool IsSemicolon(const std::shared_ptr<ast::INode>& node);
+
+private:
+  static bool SameDataType(const std::shared_ptr<ast::INode>& node, ast::DataType data_type);
 };
 
 } // scc::parser::common
