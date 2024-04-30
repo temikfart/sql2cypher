ad3b01263e647611064faa5a674bc8d8feafcd06
Artyom Fartygin <fartygin.a@phystech.edu>
2024-04-30 23:51:28 +0300

    [#38] Add Cypher Node structure

---
+++ src/translator/cypher/graph/node.cpp
@@ -0,0 +1,90 @@
+#include <utility>
+
+#include "SCC/translator/cypher/graph/node.h"
+
+namespace scc::translator::cypher {
+
+Label::Label(std::string name)
+    : value(std::move(name)) {}
+
+std::string Label::ToString() const {
+  return ":" + value;
+}
+
+std::ostream& operator<<(std::ostream& os, const Label& label) {
+  os << label.ToString();
+  return os;
+}
+
+NodeProperty::NodeProperty(std::string  name, std::string value, PropertyType type)
+    : name(std::move(name)), value(std::move(value)), type(type) {}
+
+std::string NodeProperty::ToString() const {
+  bool is_string = type == PropertyType::kString;
+  auto val = is_string ? ("\"" + value + "\"") : value;
+  return name + ": " + val;
+}
+
+std::ostream& operator<<(std::ostream& os, const NodeProperty &property) {
+  os << property.ToString();
+  return os;
+}
+
+Node::Node(Label label)
+    : labels({std::move(label)}) {}
+Node::Node(Label label, std::set<NodeProperty> properties)
+    : labels({std::move(label)}), properties(std::move(properties)) {}
+Node::Node(Label label, std::set<NodeProperty> properties, std::string variable)
+    : labels({std::move(label)}), properties(std::move(properties)),
+      variable(std::move(variable)) {}
+
+Node& Node::AddProperty(const NodeProperty& property) {
+  properties.insert(property);
+  return *this;
+}
+Node& Node::AddProperty(const std::string& name, const std::string& value, PropertyType type) {
+  properties.insert(NodeProperty(name, value, type));
+  return *this;
+}
+Node& Node::AddLabel(const Label& label) {
+  labels.insert(label);
+  return *this;
+}
+Node& Node::AddLabel(const std::string& label) {
+  labels.insert(Label(label));
+  return *this;
+}
+Node& Node::SetVariable(const std::string& variable) {
+  this->variable = variable;
+  return *this;
+}
+
+std::string Node::ToString() const {
+  std::stringstream ss;
+  ss << "(";
+  if (!variable.empty()) {
+    ss << variable;
+  }
+  for (const auto& label : labels) {
+    ss << label.ToString();
+  }
+  if (!properties.empty()) {
+    ss << " {";
+    for (auto it = properties.begin(); it != properties.end(); ++it) {
+      ss << it->ToString();
+      if (it != properties.end()) {
+        ss << ", ";
+      }
+    }
+    ss << "}";
+  }
+  ss << ")";
+  return ss.str();
+}
+
+std::ostream& operator<<(std::ostream& os, const Node& node) {
+  os << node.ToString();
+  return os;
+}
+
+} // scc::translator::cypher
---
+++ src/translator/cypher/graph/property_types.cpp
@@ -0,0 +1,46 @@
+#include "SCC/translator/cypher/graph/property_types.h"
+
+namespace scc::translator::cypher {
+
+PropertyType::PropertyType(Value value) {
+  if (value > kString)
+    throw std::invalid_argument("Incorrect value for Cypher Property Type");
+  this->value_ = value;
+}
+PropertyType::PropertyType(const std::string_view& str_type) {
+  std::string type = scc::common::LowerCase(std::string(str_type));
+  if (type == kPT_Unknown)
+    this->value_ = kUnknown;
+  else if (type == kPT_Boolean)
+    this->value_ = kBoolean;
+  else if (type == kPT_Float)
+    this->value_ = kFloat;
+  else if (type == kPT_Integer)
+    this->value_ = kInteger;
+  else if (type == kPT_String)
+    this->value_ = kString;
+  else
+    throw std::invalid_argument("No Cypher Property Type for '" + std::string(str_type) + "'");
+}
+
+std::string PropertyType::ToString() const {
+  switch (value_) {
+    case kUnknown:
+      return std::string(kPT_Unknown);
+    case kBoolean:
+      return std::string(kPT_Boolean);
+    case kFloat:
+      return std::string(kPT_Float);
+    case kInteger:
+      return std::string(kPT_Integer);
+    case kString:
+      return std::string(kPT_String);
+  }
+}
+
+std::ostream& operator<<(std::ostream& os, const PropertyType& type) {
+  os << type.ToString();
+  return os;
+}
+
+} // scc::translator::cypher
