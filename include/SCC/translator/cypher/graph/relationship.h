dfc13c8a33e0473080c1a89b48d161e63ee6606b
Artyom Fartygin <fartygin.a@phystech.edu>
2024-05-01 13:24:31 +0300

    [#38] Add Cypher Relationship structure

---
+++ include/SCC/translator/cypher/graph/relationship.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <ostream>
+#include <set>
+#include <sstream>
+#include <string>
+#include <utility>
+
+#include "SCC/translator/cypher/graph/node.h"
+
+namespace scc::translator::cypher {
+
+struct Relationship {
+  enum class Direction {
+    kLeft,
+    kRight,
+    kBoth,
+  };
+
+  std::string type;
+  Node start;
+  Node end;
+  Direction direction;
+  std::string variable;
+//  std::set<NodeProperty> properties;
+
+  explicit Relationship(std::string type, Node start, Node end,
+                        Direction direction = Direction::kRight);
+  explicit Relationship(std::string type, Node start, Node end, std::string variable,
+                        Direction direction = Direction::kRight);
+
+  std::string ToString() const;
+
+private:
+  std::string GetLeftArrow() const;
+  std::string GetRightArrow() const;
+};
+
+std::ostream& operator<<(std::ostream& os, const Relationship& rel);
+
+} // scc::translation::cypher
