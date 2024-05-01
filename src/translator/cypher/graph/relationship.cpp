6877f6c8aa748f01de3b239f39154a832b006fe3
Artyom Fartygin <fartygin.a@phystech.edu>
2024-05-01 13:24:31 +0300

    [#38] Add Cypher Relationship structure

---
+++ src/translator/cypher/graph/relationship.cpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "SCC/translator/cypher/graph/relationship.h"
+
+namespace scc::translator::cypher {
+
+Relationship::Relationship(std::string type, Node start, Node end, Direction direction)
+    : type(type), start(std::move(start)), end(std::move(end)), direction(direction) {}
+Relationship::Relationship(std::string type, Node start, Node end, std::string variable,
+                           Direction direction)
+    : type(type), start(std::move(start)), end(std::move(end)), direction(direction),
+      variable(std::move(variable)) {}
+
+std::string Relationship::ToString() const {
+  std::stringstream ss;
+  ss << start.ToString() << GetLeftArrow();
+  ss << "[" << variable << ":" << type << "]";
+  ss << GetRightArrow() << end.ToString();
+}
+
+std::string Relationship::GetLeftArrow() const {
+  if (direction == Direction::kBoth || direction == Direction::kLeft)
+    return "<-";
+  return "-";
+}
+std::string Relationship::GetRightArrow() const {
+  if (direction == Direction::kBoth || direction == Direction::kRight)
+    return "->";
+  return "-";
+}
+
+std::ostream& operator<<(std::ostream& os, const Relationship& relationship) {
+  os << relationship.ToString();
+  return os;
+}
+
+} // scc::translator::cypher
