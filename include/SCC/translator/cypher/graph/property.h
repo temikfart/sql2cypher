6687783297c75861f45783c99dbae365b0f5768a
Artyom Fartygin <fartygin.a@phystech.edu>
2024-06-02 23:32:40 +0300

    [#52] Add constraints to property structure

--- include/SCC/translator/cypher/graph/property.h
+++ include/SCC/translator/cypher/graph/property.h
@@ -7,6 +7,7 @@
 #include <vector>
 #include <utility>
 
+#include "SCC/translator/cypher/graph/constraint_types.h"
 #include "SCC/translator/cypher/graph/property_types.h"
 
 namespace scc::translator::cypher {
@@ -17,8 +18,18 @@ struct Property {
   PropertyType type;
 
   explicit Property(std::string name, std::string value, PropertyType type);
+  explicit Property(std::string name, std::string value, PropertyType type,
+                    std::vector<ConstraintType> constraints);
+
+  void AddConstraint(ConstraintType constraint);
+  void RemoveConstraint(ConstraintType constraint);
+  bool MustBeUnique() const;
+  bool MustBeNotNull() const;
 
   std::string ToString() const;
+
+private:
+  std::vector<ConstraintType> constraints;
 };
 
 std::ostream& operator<<(std::ostream& os, const Property& property);
--- src/translator/cypher/graph/property.cpp
+++ src/translator/cypher/graph/property.cpp
@@ -4,6 +4,30 @@ namespace scc::translator::cypher {
 
 Property::Property(std::string  name, std::string value, PropertyType type)
     : name(std::move(name)), value(std::move(value)), type(type) {}
+Property::Property(std::string name, std::string value, PropertyType type,
+                   std::vector<ConstraintType> constraints)
+    : name(std::move(name)), value(std::move(value)), type(type),
+      constraints(std::move(constraints)) {}
+
+void Property::AddConstraint(ConstraintType constraint) {
+  if (std::find(constraints.begin(), constraints.end(), constraint) == constraints.end()) {
+    constraints.push_back(constraint);
+  }
+}
+void Property::RemoveConstraint(ConstraintType constraint) {
+  auto it = std::find(constraints.begin(), constraints.end(), constraint);
+  if (it != constraints.end()) {
+    constraints.erase(it);
+  }
+}
+bool Property::MustBeUnique() const {
+  return std::any_of(constraints.begin(), constraints.end(),
+                     [](ConstraintType c) { return c == ConstraintType::kUniqueness; });
+}
+bool Property::MustBeNotNull() const {
+  return std::any_of(constraints.begin(), constraints.end(),
+                     [](ConstraintType c) { return c == ConstraintType::kExistence; });
+}
 
 std::string Property::ToString() const {
   return name + ": " + value;
