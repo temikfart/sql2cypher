80bebb5fe16944b38d31c6f759998c43a4341877
Artyom Fartygin <fartygin.a@phystech.edu>
2024-06-09 00:37:07 +0300

    [#55] Refactor enum serialization tests

--- test/fixtures/include/SCC/fixtures_common/serialization.h
+++ test/fixtures/include/SCC/fixtures_common/serialization.h
@@ -24,3 +24,36 @@ template<typename T>
 inline json create_enum_json(T type) {
   return json::parse(create_enum_json_string(type));
 }
+
+template<typename T>
+inline void EnumToJsonTestBody(const T& type, const std::string_view& expected) {
+  ASSERT_NO_THROW(
+      json type_json = type;
+      EXPECT_EQ(expected, type_json);
+  );
+}
+template<typename T>
+inline void EnumFromJsonTestBody(const std::string_view& json_str, const T& expected) {
+  ASSERT_NO_THROW(
+      json type_json = json_str;
+      EXPECT_EQ(expected, type_json.template get<T>());
+  );
+}
+
+template<typename T>
+inline void EnumClassToJsonTestBody(const T& type) {
+  ASSERT_NO_THROW(
+      json type_json;
+      to_json(type_json, type);
+      EXPECT_EQ(create_enum_json_string(type), dump(type_json));
+  );
+}
+template<typename T>
+inline void EnumClassFromJsonTestBody(const T& type) {
+  ASSERT_NO_THROW(
+      T type_from_json;
+      json type_json = create_enum_json(type);
+      from_json(type_json, type_from_json);
+      EXPECT_EQ(type, type_from_json);
+  );
+}
--- test/translator/cypher/graph/constraint_types_serialization_test.cpp
+++ test/translator/cypher/graph/constraint_types_serialization_test.cpp
@@ -12,91 +12,23 @@ using namespace testing;
 using nlohmann::json;
 
 TEST(CypherConstraintTypeSerializationTests, EnumToJsonTest) {
-  ASSERT_NO_THROW(
-      json none_json = ConstraintType::Value::kNone;
-      EXPECT_EQ(kCT_None, none_json);
-  );
-
-  ASSERT_NO_THROW(
-      json unique_json = ConstraintType::Value::kUniqueness;
-      EXPECT_EQ(kCT_Uniqueness, unique_json);
-  );
-
-  ASSERT_NO_THROW(
-      json not_null_json = ConstraintType::Value::kExistence;
-      EXPECT_EQ(kCT_Existence, not_null_json);
-  );
+  EnumToJsonTestBody<ConstraintType::Value>(ConstraintType::Value::kNone, kCT_None);
+  EnumToJsonTestBody<ConstraintType::Value>(ConstraintType::Value::kUniqueness, kCT_Uniqueness);
+  EnumToJsonTestBody<ConstraintType::Value>(ConstraintType::Value::kExistence, kCT_Existence);
 }
 TEST(CypherConstraintTypeSerializationTests, EnumFromJsonTest) {
-  ASSERT_NO_THROW(
-      json none_json = kCT_None;
-      EXPECT_EQ(ConstraintType::Value::kNone,
-                none_json.template get<ConstraintType::Value>());
-  );
-
-  ASSERT_NO_THROW(
-      json unique_json = kCT_Uniqueness;
-      EXPECT_EQ(ConstraintType::Value::kUniqueness,
-                unique_json.template get<ConstraintType::Value>());
-  );
-
-  ASSERT_NO_THROW(
-      json not_null_json = kCT_Existence;
-      EXPECT_EQ(ConstraintType::Value::kExistence,
-                not_null_json.template get<ConstraintType::Value>());
-  );
+  EnumFromJsonTestBody<ConstraintType::Value>(kCT_None, ConstraintType::Value::kNone);
+  EnumFromJsonTestBody<ConstraintType::Value>(kCT_Uniqueness, ConstraintType::Value::kUniqueness);
+  EnumFromJsonTestBody<ConstraintType::Value>(kCT_Existence, ConstraintType::Value::kExistence);
 }
 
 TEST(CypherConstraintTypeSerializationTests, ToJsonTest) {
-  ASSERT_NO_THROW(
-      ConstraintType none_type = ConstraintType::kNone;
-
-      json none_json;
-      to_json(none_json, none_type);
-      EXPECT_EQ(create_enum_json_string(none_type), dump(none_json));
-  );
-
-  ASSERT_NO_THROW(
-      ConstraintType unique_type = ConstraintType::kUniqueness;
-
-      json unique_json;
-      to_json(unique_json, unique_type);
-      EXPECT_EQ(create_enum_json_string(unique_type), dump(unique_json));
-  );
-
-  ASSERT_NO_THROW(
-      ConstraintType not_null_type = ConstraintType::kExistence;
-
-      json not_null_json;
-      to_json(not_null_json, not_null_type);
-      EXPECT_EQ(create_enum_json_string(not_null_type), dump(not_null_json));
-  );
+  EnumClassToJsonTestBody<ConstraintType>(ConstraintType::kNone);
+  EnumClassToJsonTestBody<ConstraintType>(ConstraintType::kUniqueness);
+  EnumClassToJsonTestBody<ConstraintType>(ConstraintType::kExistence);
 }
 TEST(CypherConstraintTypeSerializationTests, FromJsonTest) {
-  ASSERT_NO_THROW(
-      ConstraintType none_type = ConstraintType::kNone;
-      ConstraintType none_type_from_json;
-
-      json none_json = create_enum_json(none_type);
-      from_json(none_json, none_type_from_json);
-      EXPECT_EQ(none_type, none_type_from_json);
-  );
-
-  ASSERT_NO_THROW(
-      ConstraintType unique_type = ConstraintType::kUniqueness;
-      ConstraintType unique_type_from_json;
-
-      json unique_json = create_enum_json(unique_type);
-      from_json(unique_json, unique_type_from_json);
-      EXPECT_EQ(unique_json, unique_type_from_json);
-  );
-
-  ASSERT_NO_THROW(
-      ConstraintType not_null_type = ConstraintType::kExistence;
-      ConstraintType not_null_type_from_json;
-
-      json not_null_json = create_enum_json(not_null_type);
-      from_json(not_null_json, not_null_type_from_json);
-      EXPECT_EQ(not_null_json, not_null_type_from_json);
-  );
+  EnumClassFromJsonTestBody<ConstraintType>(ConstraintType::kNone);
+  EnumClassFromJsonTestBody<ConstraintType>(ConstraintType::kUniqueness);
+  EnumClassFromJsonTestBody<ConstraintType>(ConstraintType::kExistence);
 }
