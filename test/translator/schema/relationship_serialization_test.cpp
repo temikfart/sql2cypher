874e0d1446b273a3c72e0ad28288d139a6b19602
Artyom Fartygin <fartygin.a@phystech.edu>
2024-06-09 13:11:08 +0300

    [#55] Add missed templates in tests

--- test/translator/schema/relationship_serialization_test.cpp
+++ test/translator/schema/relationship_serialization_test.cpp
@@ -11,37 +11,37 @@ using nlohmann::json;
 
 TEST(SchemaApplyConditionSerializationTests, ToJsonTest) {
   ApplyCondition empty_condition;
-  ObjectToJsonTestBody(empty_condition, create_json_string(empty_condition));
+  ObjectToJsonTestBody<ApplyCondition>(empty_condition, create_json_string(empty_condition));
 
   ApplyCondition condition(5, 8);
-  ObjectToJsonTestBody(condition, create_json_string(condition));
+  ObjectToJsonTestBody<ApplyCondition>(condition, create_json_string(condition));
 }
 TEST(SchemaApplyConditionSerializationTests, FromJsonTest)  {
   ApplyCondition empty_condition;
-  ObjectFromJsonTestBody(create_json(empty_condition), empty_condition);
+  ObjectFromJsonTestBody<ApplyCondition>(create_json(empty_condition), empty_condition);
 
   ApplyCondition condition(4, 2);
-  ObjectFromJsonTestBody(create_json(condition), condition);
+  ObjectFromJsonTestBody<ApplyCondition>(create_json(condition), condition);
 }
 
 TEST(SchemaRelationshipSerializationTests, ToJsonTest) {
   Relationship empty_relationship;
-  ObjectToJsonTestBody(empty_relationship, create_json_string(empty_relationship));
+  ObjectToJsonTestBody<Relationship>(empty_relationship, create_json_string(empty_relationship));
 
   Relationship relationship("LOVES", "Maxim", "Katya");
-  ObjectToJsonTestBody(relationship, create_json_string(relationship));
+  ObjectToJsonTestBody<Relationship>(relationship, create_json_string(relationship));
 
   relationship.AddCondition(ApplyCondition(1, 2));
   relationship.AddCondition(ApplyCondition(5, 7));
-  ObjectToJsonTestBody(relationship, create_json_string(relationship));
+  ObjectToJsonTestBody<Relationship>(relationship, create_json_string(relationship));
 }
 TEST(SchemaRelationshipSerializationTests, FromJsonTest)  {
   Relationship empty_relationship;
-  ObjectFromJsonTestBody(create_json(empty_relationship), empty_relationship);
+  ObjectFromJsonTestBody<Relationship>(create_json(empty_relationship), empty_relationship);
 
   Relationship relationship("DIRECTED", "Nolan", "Batman");
-  ObjectFromJsonTestBody(create_json(relationship), relationship);
+  ObjectFromJsonTestBody<Relationship>(create_json(relationship), relationship);
 
   relationship.AddCondition(ApplyCondition(9, 0));
-  ObjectFromJsonTestBody(create_json(relationship), relationship);
+  ObjectFromJsonTestBody<Relationship>(create_json(relationship), relationship);
 }
