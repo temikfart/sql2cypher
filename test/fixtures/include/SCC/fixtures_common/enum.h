e41f857cbd229fe1e98d8ffae822decd5c886d94
Artyom Fartygin <fartygin.a@phystech.edu>
2024-07-28 21:55:23 +0300

    [#61] Add enum test bodies as fixtures

---
+++ test/fixtures/include/SCC/fixtures_common/enum.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <type_traits>
+#include <string>
+#include <sstream>
+
+template<typename EnumBaseType, typename EnumT, typename = std::enable_if_t<std::is_enum_v<EnumT>>>
+void CastEnumTestBody(int n) {
+  EXPECT_EQ(static_cast<EnumBaseType>(n), EnumT(n));
+}
+
+template<typename EnumWrapperT, typename EnumT, typename = std::enable_if_t<std::is_enum_v<EnumT>>>
+void CastWrapperToValueTestBody(EnumWrapperT element) {
+  EXPECT_EQ(element, static_cast<EnumT>(EnumWrapperT(element)));
+}
+
+template<typename EnumWrapperT, typename EnumT, typename = std::enable_if_t<std::is_enum_v<EnumT>>>
+void CreateWithInvalidArgumentTestBody(int v) {
+  EXPECT_THROW(EnumWrapperT(EnumT(v)), std::invalid_argument);
+}
+
+template<typename EnumWrapperT, typename EnumT, typename = std::enable_if_t<std::is_enum_v<EnumT>>>
+void CreateWithInvalidArgumentTestBody(const std::string& v) {
+  EXPECT_THROW(EnumWrapperT element(v), std::invalid_argument);
+}
+
+template<typename EnumWrapperT>
+void CreateFromStringTestBody(const std::string_view& str, EnumWrapperT expected) {
+  EnumWrapperT element(str);
+  EXPECT_EQ(expected, element);
+}
+
+template<typename EnumWrapperT>
+void ToStringTestBody(EnumWrapperT element, const std::string_view& expected) {
+  EXPECT_EQ(expected, element.ToString());
+}
+
+template<typename EnumWrapperT, typename EnumT, typename = std::enable_if_t<std::is_enum_v<EnumT>>>
+void OutputTestBody(EnumT element, const std::string_view& expected) {
+  std::ostringstream oss;
+  oss << EnumWrapperT(element);
+  EXPECT_EQ(expected, oss.str());
+}
